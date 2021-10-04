b5db085ded69a5b0f9bcc1216931effef1f354f3
dCrowb <oishevchenko94@gmail.com>
2021-10-04 22:13:04 +0300

    add main_vm

---
+++ main_vm.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("/home/crow/.terraform.d/credentials.tfrc.json")
+  project     = "solid-idiom-327007"
+  region      = "europe-west4"
+  zone        = "europe-west4-a"
+}
+
+resource "google_compute_instance" "default" {
+  name         = "test"
+  machine_type = "e2-micro"
+  zone         = "europe-west4-a"
+
+  tags = ["foo", "bar"]
+
+  boot_disk {
+    initialize_params {
+      image = "centos-cloud/centos-8"
+    }
+  }
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+}
+
